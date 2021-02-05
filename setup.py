ed4fbd69b890440b7e7e297f69abda0bdd2007f3
Captmoonshot <gygantor@protonmail.com>
2021-02-04 19:12:04 -0800

    bumpversion to 1.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
 	name="pydango-pip",
-    version="1.0.1",
+    version="1.0.2",
     description="Pip installable database schema that attempts to mimic the Fandango database",
     long_description=README,
     long_description_content_type="text/markdown",
